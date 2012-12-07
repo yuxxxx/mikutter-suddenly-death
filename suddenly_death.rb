1cf0fc3e7b444a4a713aa26544322797142f729a
yuxxxx <yuunnxx@gmail.com>
2012-12-08 07:43:41 +0900

    とりあえず投稿するときのコマンドと同じ感じにしてみた

--- suddenly_death.rb
+++ suddenly_death.rb
@@ -4,8 +4,9 @@ require './suddenly_death_module.rb'
 Plugin.create(:suddenly_death) do
  command(:suddenly_death,
          name: '突然の死',
-         condition: lambda{ |opt| true },
-         visible: true,
+         condition: Plugin::Command[:Editable],
+         visible: false,
+         icon: MUI:Skin.get("post.png"),
          role: :postbox) do |opt|
     opt.buffer.text = SuddenlyDeath.wrap opt.buffer.text
   end
